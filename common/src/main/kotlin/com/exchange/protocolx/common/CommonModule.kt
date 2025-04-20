878501fb1e8f42750e3ce0f3ebce7a7103561bc6
Nhahan <kisy324@naver.com>
2025-04-20 20:59:14 +0900

    feat: add common module with shared dependencies

---
+++ common/src/main/kotlin/com/exchange/protocolx/common/CommonModule.kt
@@ -0,0 +1,8 @@
+package com.exchange.protocolx.common
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan("com.exchange.protocolx.common")
+class CommonModule 
