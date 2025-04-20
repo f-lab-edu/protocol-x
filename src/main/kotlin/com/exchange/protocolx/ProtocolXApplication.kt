d1b5f98c88ac3aefdaaefeb9891da6fb13c13438
Nhahan <kisy324@naver.com>
2025-04-20 18:40:44 +0900

    init

---
+++ src/main/kotlin/com/exchange/protocolx/ProtocolXApplication.kt
@@ -0,0 +1,11 @@
+package com.exchange.protocolx
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProtocolXApplication
+
+fun main(args: Array<String>) {
+    runApplication<ProtocolXApplication>(*args)
+}
---
+++ src/test/kotlin/com/exchange/protocolx/ProtocolXApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.exchange.protocolx
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProtocolXApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
