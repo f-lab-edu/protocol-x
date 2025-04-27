e0366d322b0ebe29e56d5ff71497b6e8af940b99
Nhahan <kisy324@naver.com>
2025-04-27 10:08:24 +0900

    fix: add actuator

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -13,6 +13,7 @@ dependencyManagement {
 
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
--- user-auth-service/build.gradle.kts
+++ user-auth-service/build.gradle.kts
@@ -8,12 +8,13 @@ plugins {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.auth0:java-jwt:4.5.0")
     runtimeOnly("com.mysql:mysql-connector-j")
-    
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
 }
