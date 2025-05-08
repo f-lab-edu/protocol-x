ee5a222306b6e1ae3ce74b18270d154f441f4537
Nhahan <kisy324@naver.com>
2025-05-08 23:57:37 +0900

    refactor(matching-engine): Adjust application entrypoint after service removal

--- matching-engine/src/main/kotlin/com/exchange/protocolx/matchingengine/MatchingEngineApplication.kt
+++ matching-engine/src/main/kotlin/com/exchange/protocolx/matchingengine/MatchingEngineApplication.kt
@@ -1,12 +1,9 @@
 package com.exchange.protocolx.matchingengine
 
 import com.exchange.protocolx.matchingengine.runtime.ApplicationLifecycle
-// import com.exchange.protocolx.matchingengine.service.MatchingEngineService
 
 // 임시
 fun main() {
-    // val matchingEngineService = MatchingEngineService()
-    
     val lifecycle = ApplicationLifecycle()
     lifecycle.waitForShutdown()
 } 
